3a18a8517d5f5e62e5228688bffe66a109e3f60c
RafiGabizon <rafigabizon11@gmail.com>
2025-03-27 09:34:33 +0200

    build and run fixes

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
-const cors = require('cors');
-const connectDB = require('./config/db');
-const { createInitialCodeBlocks } = require('./controllers/codeBlockController');
-const { initializeSocketHandlers } = require('./sockets/socketHandlers');
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+const connectDB = require("./config/db");
+const { createInitialCodeBlocks } = require("./controllers/codeBlockController");
+const { initializeSocketHandlers } = require("./sockets/socketHandlers");
 
 connectDB();
 createInitialCodeBlocks();
@@ -12,20 +12,20 @@ createInitialCodeBlocks();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 
 // Routes
-app.use('/api/codeblocks', require('./routes/codeBlockRoutes'));
+app.use("/api/codeblocks", require("./routes/codeBlockRoutes"));
 
 const server = http.createServer(app);
 
 // Initialize Socket.IO
 const io = new Server(server, {
   cors: {
-    origin: '*',
-    methods: ['GET', 'POST']
-  }
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
 });
 
 // Setup socket handlers
@@ -34,3 +34,5 @@ initializeSocketHandlers(io);
 // Start server
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+module.exports = { app, server };
